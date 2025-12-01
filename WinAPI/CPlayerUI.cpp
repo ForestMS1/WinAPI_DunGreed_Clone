60f5711a5525243ad8a302b2907edd7ac030c2fe
daesung <dasung56@gmail.com>
2025-12-01 22:11:32 +0900

    1201 자습실 마무리

---
+++ WinAPI/CPlayerUI.cpp
@@ -0,0 +1,10 @@
+#include "pch.h"
+#include "CPlayerUI.h"
+
+CPlayerUI::CPlayerUI()
+{
+}
+
+CPlayerUI::~CPlayerUI()
+{
+}
---
+++ WinAPI/CPlayerUI.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "CUI.h"
+class CPlayerUI :
+    public CUI
+{
+    CPlayerUI();
+    virtual ~CPlayerUI();
+};
+
